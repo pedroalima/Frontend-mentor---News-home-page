969907f8a847bd39149780cd60ae8762ac3331c3
Pedro Lima <tsc.pedrolima@gmail.com>
2023-03-11 11:13:26 -0300

    Add function menuShow for mobile

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-function botao(){
-    document.querySelector('.botao__banner');
+function menuShow(){
+    const menuMobile = document.querySelector('.menu__mobile');
+
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/icon-menu.svg";
+    }
+    else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img/icon-menu-close.svg";
+    }
+
 }
 
 
